57114bae94f1ac4c256641d6e624d0deeff4e016
lemoi18 <lemoi18@uia.no>
2023-04-01 18:10:52 +0200

    Addes some imports

--- App.tsx
+++ App.tsx
@@ -1,14 +1,14 @@
-import React, { useState, useEffect } from 'react';
-import { SafeAreaView, StyleSheet, Text } from 'react-native';
-import { realmOpen, isRealmEmpty } from './backend/realm/utils.js'
-import { updateGlucose } from './backend/realm/CRUD.js';
-import { BackgroundTest } from './backend/background-fetch.js';
+import React, {useState, useEffect} from 'react';
+import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {realmOpen, isRealmEmpty} from './backend/realm/utils.js';
+import {updateGlucose} from './backend/realm/CRUD.js';
+import {BackgroundTest} from './backend/background-fetch.js';
 
 import './backend/realm/testCRUD.js';
+import {AuthenticationContextProvider} from './src/services/Auth/Auth-Context.js';
+import {Navigation} from './src/infastructure/navigation/index.js';
 
 const App = () => {
-
-
   return (
     <View style={{flex: 1}}>
       <AuthenticationContextProvider>
@@ -16,73 +16,73 @@ const App = () => {
       </AuthenticationContextProvider>
     </View>
   );
-
+};
 async function callAll() {
-    await updateGlucose();
-    await countGlucose();
+  await updateGlucose();
+  await countGlucose();
 }
 
 async function getUserById(id: number) {
-    const realm = await realmOpen();
-    const person = realm.objectForPrimaryKey('User', id);
+  const realm = await realmOpen();
+  const person = realm.objectForPrimaryKey('User', id);
 
-    if (person) {
-        console.log(`Found person with ID ${id}:`, person);
-        return person;
-    } else {
-        console.log(`Person with ID ${id} not found.`);
-        return null;
-    }
+  if (person) {
+    console.log(`Found person with ID ${id}:`, person);
+    return person;
+  } else {
+    console.log(`Person with ID ${id} not found.`);
+    return null;
+  }
 }
 
 async function getAllUsers() {
-    const realm = await realmOpen();
-    const users = await realm.objects('GlucoseInfo');
+  const realm = await realmOpen();
+  const users = await realm.objects('GlucoseInfo');
 
-    console.log('All users:', users);
-    return users;
+  console.log('All users:', users);
+  return users;
 }
 
 async function countGlucose() {
-    const realm = await realmOpen();
-    const glucoses = realm.objects('GlucoseInfo');
+  const realm = await realmOpen();
+  const glucoses = realm.objects('GlucoseInfo');
 
-    console.log(`There are ${glucoses.length} entries in the Realm file.`);
-    return glucoses.length;
+  console.log(`There are ${glucoses.length} entries in the Realm file.`);
+  return glucoses.length;
 }
 
 async function isOpenEmpty() {
-    const realm = await realmOpen();
-    const isEmpty = await isRealmEmpty(realm);
-    console.log(`Is the Realm database empty? ${isEmpty}`);
+  const realm = await realmOpen();
+  const isEmpty = await isRealmEmpty(realm);
+  console.log(`Is the Realm database empty? ${isEmpty}`);
 }
 
 async function addPerson(id: number, name: string, email: string, age: number) {
-    const realm = await realmOpen();
+  const realm = await realmOpen();
 
-    realm.write(() => {
-        realm.create('User', {
-            id: id,
-            name: name,
-            email: email,
-            Age: age,
-        });
+  realm.write(() => {
+    realm.create('User', {
+      id: id,
+      name: name,
+      email: email,
+      Age: age,
     });
+  });
 
-    console.log('Person added to the Realm file');
+  console.log('Person added to the Realm file');
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    text: {
-        fontSize: 20,
-        fontWeight: 'bold',
-    },
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  text: {
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
 });
 
 export default App;
